a87ff07c30573f8c4d2e090117c1ad465b5fa7ba
rkn2 <rkn2@princeton.edu>
2017-10-20 13:09:37 -0400

    cant get just the needed text yet

--- find_small_blocks.py
+++ find_small_blocks.py
@@ -1,27 +1,42 @@
 #finds and deletes small blocks
 
 #calculate volumne 
-#import glob
+
 import os
+
 
 directory = 'C:/Users/Rebecca Napolitano/Google Drive/Documents/Research/mikehess/palazzo vecchio/2017_9_7_ElementiModels/FoundationModels/ExistingGeometry/'
 filename ='testy.txt'
+outFile = 'blockstesty.txt'
+outFile2 = 'thisisatest.txt'
 os.chdir(directory)
 
+outfile = open(outFile, "a+")
+outfile2 = open(outFile2,"a+")
+
+#count blocks
+total = 0
+
+with open(filename) as f:
+    for line in f:
+        finded = line.find('block')
+        if finded != -1 and finded != 0:
+            total += 1
+f.close()
+
 #read in the gvol file
-dataFile = open(filename, 'r')
-data = dataFile.read()
-dataFile.close()
+data = open(filename).read().split('\n')
 
-
-#get the section for the first block
-start = '; block'
-end = ';block'
-numberBlocks = data.find(start)
-for i = 1: numberBlocks:
-    data.find(start)
-
-
+#find discrete block sections
+#change '   ' to ';;;'
+for line in data:
+    line= line.replace('   ', ';;;')
+    outfile.write(line + '\n')
+#throw away any lines that do not start with ';;;'
+for line in data: 
+    if line.find(';;;') !=0:
+        outfile2.write(line + '\n')
+              
 
 #find unique vertex locations
 #find volume of that block
@@ -31,3 +46,5 @@ for i = 1: numberBlocks:
 #find triple product 
 #find 1/6 absolute value of the triple product
 
+outfile.close()
+outfile2.close()
