ded567b1bb1652c4f80593d72517e3e4d6b2fbff
rkn2 <rkn2@princeton.edu>
2017-10-24 15:40:09 -0400

    works! deletes blocks with volume under a certain tolerance

--- find_small_rigid_blocks.py
+++ find_small_rigid_blocks.py
@@ -1,25 +1,25 @@
 import rhinoscriptsyntax as rs
 
+tolerance = 170
 
-tolerance = 170 #m3
-
-
-#pick an object
 object_id = rs.GetObjects()
 
-#Loop between my objects
 for object_i in object_id:
     #deselect any objects
     rs.Command("-_selnone")
+    
     #get block id
-    #print object_i
     object_i = str(object_i)
     block = rs.Command("-_SelID " + object_i)
-    #get volume
-    volume = rs.Command("-_Volume ")
-    print volume
-#    #delete if too small
-#    if volume < tolerance:
-#        rs.Command("-_Delete")
-#        print "Deleting block"
-#    
+    
+    #getvolume
+    i = rs.coercebrep(object_i)
+
+    blockVolume = i.GetVolume()
+    
+    #delete where appropriate
+    if blockVolume < tolerance:
+        rs.Command("-_Delete")
+        print "Deleting block"
+    
+    #print type(blockVolume)
