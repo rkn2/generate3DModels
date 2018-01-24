f588a48658d5a8a19f487dda2d5463e8cea0de91
rkn2 <rkn2@princeton.edu>
2018-01-24 14:12:27 -0500

    meshes only deformable ones, outputs rigid

--- get_layers.py
+++ get_layers.py
@@ -1,30 +1,83 @@
 import rhinoscriptsyntax as rs
+import os
+import shutil
 
 directory = 'G:\\My Drive\\Documents\\Research\\mikehess\\palazzo vecchio\\2017_9_7_ElementiModels\\FoundationModels\\ExistingGeometry\\2017_10_30\\'
 fileName = 'this_is_a_test'
 filetype = '.wrl'
+meshValue = '10 '
+mesh = ['1']
+
+angle = ' _angle=0'
+aspectratio =' _aspectratio=0' 
+distance = ' _distance=0'
+grid = ' _grid=0'
+maxedgelength = ' _maxedgelength=' + meshValue
+maxedgelengthstr= maxedgelength
+minedgelength = ' _minedgelength=' + meshValue
+minedgelengthstr = minedgelength
+
+advOptions = angle + aspectratio + distance + grid + maxedgelengthstr + minedgelengthstr
+
+#gsurfcommands
+
+mode = '_mode=tri'
+minedgelength = '_minedgelength=' + meshValue
+maxedgelength = '_maxedgelength=' + meshValue
+ridgeangle = '_ridgeangle=20'
+maxgradation = '_maxgradation=1'
+deleteinput = '_deleteinput=Yes'
+
+gsurfOptions = mode + minedgelength + maxedgelength + ridgeangle + maxgradation + deleteinput
+
+#gvol options
+gvol_mode = '_mode=tet '
+outputformat = '_outputformat=3DEC '
+
+gvolOptions = gvol_mode + outputformat
 
 
 #get layers
 layers = rs.LayerNames()
-rs.Command("Show")
+
+os.chdir(directory)
+
 for layer in layers:
-    #grab only that layer
-    rs.Command("-_SelLayer " + layer)
-    rs.Command("-_Invert")
-    rs.Command("Hide Enter")
-    rs.Command("-_SelAll")
+    rs.Command("Show")
+    rs.Command("-_SelNone ")
     
-    #make cmdstr for export
-    path = "\"" + directory + fileName + "_" + layer + filetype + "\""
-    
-    cmdstr = "-_Export " + path
-    if filetype == '.wrl':
-        cmdstr += " Enter Enter"
-    
-    cmd = rs.Command(cmdstr)
-    if not (cmd):
-        success = False
-    
-    rs.Command("-_SelNone")
-    rs.Command("Show ")
+    if layer in mesh:
+        #run mesh, gsurf, gvol        
+        rs.Command("-_SelLayer " + layer)
+        rs.Command("Invert ")
+        rs.Command("Hide Enter ")
+        rs.Command("-_SelLayer " + layer)
+        rs.Command("-_Mesh _DetailedOptions _AdvancedOptions " + advOptions + '_Enter _Enter')
+        rs.Command("-_SelNone ")
+        rs.Command("-_SelMesh ")
+        rs.Command("-_Gsurf " + gsurfOptions + '_Enter _Enter')
+        rs.Command("-_SelMesh")
+        rs.Command("-_Gvol " + gvolOptions + '_Enter _Enter') 
+        #rename gvol
+        saveName = fileName + "_" + layer + ".3ddat"
+        shutil.move('GVol.3ddat', saveName)
+        
+        
+    else:
+        #grab only that layer
+        rs.Command("-_SelLayer " + layer)
+        rs.Command("-_Invert")
+        rs.Command("Hide Enter")
+        rs.Command("-_SelAll")
+        
+        #make cmdstr for export
+        path = "\"" + directory + fileName + "_" + layer + filetype + "\""    
+        cmdstr = "-_Export " + path
+        if filetype == '.wrl':
+            cmdstr += " Enter Enter"   
+        cmd = rs.Command(cmdstr)
+        if not (cmd):
+            success = False   
+        rs.Command("-_SelNone")
+        rs.Command("Show ")
+        
